74a6fa1fa50bcd7c29e38eeaba84937ce04bfcef
Joao Romao <tupi_1800@hotmail.com>
2025-10-14 21:58:22 -0300

    Criando a Grid responsiva de PostList com Tailwind CSS e nossos componentes

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,11 +1,30 @@
 import { postRepository } from '@/repositories/post/json-post-repositoy';
+import { PostCoverImage } from '../PostCoverImage';
+import { PostHeading } from '../PostHeading';
 
 export default async function PostList() {
   const posts = await postRepository.findAll();
   return (
-    <div>
+    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
       {posts.map(post => (
-        <p key={post.id}>{post.title}</p>
+        <div className='flex flex-col group gap-4' key={post.id}>
+          <PostCoverImage
+            linkProps={{ href: `/post/${post.slug}` }}
+            imageProps={{ width: 1200, height: 720, src: post.coverImageUrl, alt: post.title, priority: true }}
+          />
+
+          <div className='flex flex-col gap-4 sm:justify-center'>
+            <time className='text-slate-600 block text-sm/tight' dateTime={post.createdAt}>
+              {post.createdAt}
+            </time>
+
+            <PostHeading url='#' as='h2'>
+              {post.title}
+            </PostHeading>
+
+            <p>{post.excerpt}</p>
+          </div>
+        </div>
       ))}
     </div>
   );
