c4a656b70c8711d3802ab33e3960099e24a46e03
Joao Romao <tupi_1800@hotmail.com>
2025-10-18 18:01:13 -0300

    Exibindo a data formatada em nossos componentes

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,7 @@
 import { postRepository } from '@/repositories/post/json-post-repositoy';
 import { PostCoverImage } from '../PostCoverImage';
 import { PostHeading } from '../PostHeading';
+import { formatDistanceToNow, formatDatetime } from '@/utils/format-datetime';
 
 export default async function PostList() {
   const posts = await postRepository.findAll();
@@ -16,8 +17,12 @@ export default async function PostList() {
             />
 
             <div className='flex flex-col gap-4 sm:justify-center'>
-              <time className='text-slate-600 block text-sm/tight' dateTime={post.createdAt}>
-                {post.createdAt}
+              <time
+                className='text-slate-600 block text-sm/tight'
+                dateTime={post.createdAt}
+                title={formatDistanceToNow(post.createdAt)}
+              >
+                {formatDatetime(post.createdAt)}
               </time>
 
               <PostHeading url={postLink} as='h2'>
