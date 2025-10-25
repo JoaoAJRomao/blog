fc17c5d39c31eb89e6961646743e44e267d60804
Joao Arthur <tupi_1800@hotmail.com>
2025-10-25 17:18:16 -0300

    Configurando o Drizzle o nosso primeiro Schema (tabela da base de dados)

---
+++ src/db/seed/drizzle/schemas.ts
@@ -0,0 +1,19 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const postsTable = sqliteTable('posts', {
+  id: text('id').primaryKey(),
+  slug: text('slug').notNull().unique(),
+  title: text('title').notNull(),
+  author: text('author').notNull(),
+  excerpt: text('excerpt').notNull(),
+  content: text('content').notNull(),
+  converImageUrl: text('cover_image_url').notNull(),
+  published: integer('published', { mode: 'boolean' }).notNull().default(true),
+  createdAt: text('created_at').notNull(),
+  updatedAt: text('updated_at').notNull()
+})
+
+export type PostTableSelectMode = InferSelectModel<typeof postsTable>
+export type PostTableInsertMode = InferInsertModel<typeof postsTable>
+
