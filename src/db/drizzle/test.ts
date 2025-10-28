b982c697a212974dc8d72ac7da4945c2179bf40b
Joao Arthur <tupi_1800@hotmail.com>
2025-10-27 23:47:32 -0300

    Conexão Drizzle ORM + migrations para gerenciar as tabelas da base de dados 2

--- src/db/drizzle/test.ts
+++ src/db/drizzle/test.ts
@@ -1,6 +1,6 @@
 import { drizzleDb } from "."
 import { postsTable } from "./schemas"
-
+//npx drizzle-kit push
 (async () => {
   const posts = await drizzleDb.select().from(postsTable)
   console.log(posts)
