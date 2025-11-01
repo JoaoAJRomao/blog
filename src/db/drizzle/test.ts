1c1d3e332e02d0117f342b0986467c462c45fcfb
Joao Arthur <tupi_1800@hotmail.com>
2025-10-31 23:19:51 -0300

    Criando o repositório DrizzlePostRepository

--- src/db/drizzle/test.ts
+++ src/db/drizzle/test.ts
@@ -1,9 +1,9 @@
-import { drizzleDb } from "."
-import { postsTable } from "./schemas"
+// import { drizzleDb } from "."
+// import { postsTable } from "./schemas"
 
-(async () => {
-  const posts = await drizzleDb.select().from(postsTable);
-  posts.forEach(post => {
-    console.log(post.title);
-  })
-})();
+// (async () => {
+//   const posts = await drizzleDb.select().from(postsTable);
+//   posts.forEach(post => {
+//     console.log(post.title);
+//   })
+// })();
